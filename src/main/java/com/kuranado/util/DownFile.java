25ca250f47ac829329d88977efe53c3b209b75d3
kuranado <jingxinling1024@163.com>
2019-06-09 12:34:28 +0800

    1. 解决每个线程都请求文件所有字节的 Bug，如启 3 个线程，该 Bug 将导致请求 3 次完整的文件，导致下载所需的时间是 1 个线程所需时间的 3 倍，解决方法为利用 Http 请求头中的 Range 属性来让每个线程分块请求资源

--- src/main/java/com/kuranado/util/DownFile.java
+++ src/main/java/com/kuranado/util/DownFile.java
@@ -11,7 +11,7 @@ import java.net.URL;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * 多线程分块下载单个文件，最后合并为一个文件
+ * 多线程分块下载单个文件，使用 RandomAccessFile 保存为一个文件
  *
  * @Author: Xinling Jing
  * @Date: 2019-06-08 12:44
@@ -20,21 +20,21 @@ import java.util.concurrent.CountDownLatch;
 public class DownFile {
 
     /**
-     * 文件下载路径
+     * 文件下载 url
      */
     private URL fileUrl;
+    /**
+     * 文件总字节数
+     */
+    private int fileLength;
     /**
      * 文件下载的线程数
      */
     private int threadCount;
     /**
-     * 每个线程下载文件的开始位置
+     * 每个线程需下载文件块的字节数
      */
-    private int startPos;
-    /**
-     * 每个线程需下载文件的长度
-     */
-    private int size;
+    private int blockSize;
     /**
      * 下载的文件路径(包含文件名)
      */
@@ -58,17 +58,17 @@ public class DownFile {
     private void init() throws IOException {
         downThreads = new DownThread[threadCount];
         countDownLatch = new CountDownLatch(threadCount);
-        HttpURLConnection conn = (HttpURLConnection) fileUrl.openConnection();
-        conn.setConnectTimeout(5000);
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("connection", "keep-alive");
-        // 文件总长度
-        int fileLength = conn.getContentLength();
-        System.out.println("文件总长度:" + fileLength);
-        size = fileLength / threadCount;
-        System.out.println("每个线程大约需下载的文件长度:" + size + " Byte");
+        HttpURLConnection connection = (HttpURLConnection) fileUrl.openConnection();
+        connection.setConnectTimeout(5000);
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("connection", "keep-alive");
+        System.out.println("线程-" + Thread.currentThread().getName() + "-response heade fields:" + connection.getHeaderFields());
+        fileLength = connection.getContentLength();
+        System.out.println("文件总字节数:" + fileLength);
+        blockSize = fileLength / threadCount;
+        System.out.println("每个线程大约需下载的文件块字节数:" + blockSize);
         // 断开链接
-        conn.disconnect();
+        connection.disconnect();
     }
 
     /**
@@ -78,7 +78,11 @@ public class DownFile {
         for (int i = 0; i < threadCount; i++) {
             try {
                 RandomAccessFile randomAccessFile = new RandomAccessFile(pathName, "rw");
-                downThreads[i] = new DownThread(i * size, randomAccessFile, i);
+                if (i < threadCount - 1) {
+                    downThreads[i] = new DownThread(blockSize * i, blockSize * (i + 1) - 1, randomAccessFile, i);
+                } else {
+                    downThreads[i] = new DownThread(blockSize * i, fileLength - 1, randomAccessFile, i);
+                }
                 downThreads[i].start();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -94,9 +98,13 @@ public class DownFile {
     class DownThread extends Thread {
 
         /**
-         * 开始的位置
+         * 当前线程下载文件开始位置
          */
         private int startPos;
+        /**
+         * 当前线程下载文件结束位置
+         */
+        private int endPos;
         private InputStream inputStream;
         /**
          * RandomAccessFile 可以从文件任意位置操作文件，此处 Random 表示"任意"的意思，而不是"随机"
@@ -111,8 +119,9 @@ public class DownFile {
          */
         private int flag;
 
-        DownThread(int startPos, RandomAccessFile randomAccessFile, int flag) {
+        DownThread(int startPos, int endPos, RandomAccessFile randomAccessFile, int flag) {
             this.startPos = startPos;
+            this.endPos = endPos;
             this.randomAccessFile = randomAccessFile;
             this.flag = flag;
         }
@@ -124,20 +133,20 @@ public class DownFile {
                 connection.setRequestMethod("GET");
                 connection.setRequestProperty("connection", "keep-alive");
                 connection.setConnectTimeout(5000);
+                connection.setRequestProperty("Range", "bytes=" + startPos + "-" + endPos);
+                System.out.println("线程-" + flag + "-response heade fields:" + connection.getHeaderFields());
                 inputStream = connection.getInputStream();
-                // 跳过 startPos 个 Byte
-                inputStream.skip(startPos);
                 // 指针移到 startPos 处
                 randomAccessFile.seek(startPos);
                 // 设定缓冲区为 8 KB
                 byte[] buf = new byte[8 * 1024];
                 // 读出的字节数
                 int hasRead;
-                while (length < size && (hasRead = inputStream.read(buf)) != -1) {
+                while ((hasRead = inputStream.read(buf)) != -1) {
                     randomAccessFile.write(buf, 0, hasRead);
                     length += hasRead;
                 }
-                System.out.println("线程-" + flag + "-下载完成,下载 " + length + " Byte");
+                System.out.println("线程-" + flag + "-下载完成,下载字节数" + length);
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -155,8 +164,11 @@ public class DownFile {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         long beginTime = System.currentTimeMillis();
-        new DownFile(new URL("https://download.jetbrains.8686c.com/go/goland-2019.1.3.dmg"), 3, "/Users/jing" +
+        new DownFile(new URL("https://download.jetbrains.8686c.com/go/goland-2019.1.3.dmg"), 4, "/Users/jing" +
             "/Downloads/golang.dmg").startDown();
+        //new DownFile(new URL("http://f.hiphotos.baidu.com/image/pic/item/b151f8198618367aa7f3cc7424738bd4b31ce525" +
+        //    ".jpg"), 3, "/Users/jing/Downloads/test.jpg").startDown();
+
         // 阻塞当前线程，直到 countDownLatch 中数值为 0 后继续执行当前线程
         countDownLatch.await();
         System.out.println("下载耗时:" + (double) (System.currentTimeMillis() - beginTime) / 1000 + " s");
